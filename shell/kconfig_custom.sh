8698b84d90cc7ff0e668393b702612517d5377a9
ibrahim-13 <ib.arshad777@gmail.com>
2024-02-06 11:49:21 +0600

    update kde config

--- shell/kconfig_custom.sh
+++ shell/kconfig_custom.sh
@@ -20,20 +20,24 @@ kwriteconfig5 --file kwinrc --group NightColor --key EveningBeginFixed 1700 # se
 kwriteconfig5 --file kwinrc --group NightColor --key NightTemperature 5100 # set night temparature
 
 echo disabling kactivitymanagerd
-sudo chmod -x /usr/lib/x86_64-linux-gnu/libexec/kactivitymanagerd
-sudo chmod -x /usr/lib/x86_64-linux-gnu/qt5/plugins/kactivitymanagerd
-KACTIVITY_PID=$(ps -aux | grep kactivitymanage | grep -v grep | awk '$2 ~ /^[0-9]+$/ { print $2 }')
-if [ ! "$KACTIVITY_PID" = "" ]
-then
-    echo killing kactivitymanagerd process id $KACTIVITY_PID
-    kill -s 9 $KACTIVITY_PID
-fi
 rm -rf $HOME/.local/share/kactivitymanagerd
 touch $HOME/.local/share/kactivitymanagerd
+
+echo disabling gnome/gtk recent files
+gsettings set org.gnome.desktop.privacy remember-recent-files false
 
 echo deleting recent documents
 rm -rf $HOME/.local/share/RecentDocuments
 touch $HOME/.local/share/RecentDocuments
+rm $HOME/.local/share/recently-used.xbel
+touch $HOME/.local/share/recently-used.xbel
+sudo chattr +i $HOME/.local/share/recently-used.xbel
+
+echo deleting user places
+rm $HOME/.local/share/user-places.xbel
+touch $HOME/.local/share/user-places.xbel
+sudo chattr +i $HOME/.local/share/user-places.xbel
+
 
 echo deleting thumbnails in cache
 rm -rf ~/.cache/thumbnails
