548b27df8637b43e849bee4b291315e1b8222547
Ibrahim Khan <57107816+ibrahim-13@users.noreply.github.com>
2024-07-30 18:24:10 +0600

    [yt-playback-rate.js] add menu instead of ui

--- browser-userscripts/yt-playback-rate.js
+++ browser-userscripts/yt-playback-rate.js
@@ -2,13 +2,12 @@
 // @name        YT Playback
 // @namespace   __gh_ibrahim13_yt_playback_rate
 // @match       https://*.youtube.com/*
-// @version     2024.07.27
+// @version     2024.07.30
 // @author      github/ibrahim-13
 // @description Increase playback rate of YouTube
 // @noframes
 // @grant       GM_registerMenuCommand
-// @grant       GM_addElement
-// @grant       GM_addStyle
+// @grant       GM_unregisterMenuCommand
 // ==/UserScript==
 
 var _conf = {
@@ -18,8 +17,6 @@ var _conf = {
   },
   isEnabled: true,
   default_playback_rate: 1.5,
-  override_playback: false,
-  override_playback_rate: 1,
   preset: {
     // channel_relative_url: playback_rate,
     // ex: "/@yt": 1.25,
@@ -30,66 +27,24 @@ var _conf = {
  * UI
 **/
 
-function inject_ui() {
-  let isDragging = false;
-  GM_addStyle(`
-.__us_settings_view{background:black;color:green;font-size: 13px;border:1px solid yellow;padding:7px;position:absolute;top:0px;width:150px;height:150px;z-index:99993;display:none;}
-.__us_pb_rate{font-weight:bold;}
-.__us_btn{background:green;}
-.__us_overlay {position:absolute;cursor:pointer;top:0px;left:0px;width:100%;height:100%;z-index:99992;opacity:0.7;background:black;display:none;}
-`)
-
-  const settings = GM_addElement("div", {class: "__us_settings_view"});
-  GM_addElement(settings, "h2", { textContent: "settings"});
-  GM_addElement(settings, "label", { textContent: "monitor: "});
-
-  const settings_input_monitor = GM_addElement(settings, "input", {
-    type: "checkbox",
-    checked: _conf.isEnabled,
-  });
-  settings_input_monitor.addEventListener("change", function(e) {_conf.isEnabled = e.target.checked;});
-  GM_addElement(settings, "br", {});
-
-  GM_addElement(settings, "label", { textContent: "default speed: "});
-  const settings_span_playback_rate = GM_addElement(settings, "label", {
-    class: "__us_pb_rate",
-    textContent: String(_conf.override_playback ? _conf.override_playback_rate : _conf.default_playback_rate),
-  });
-  GM_addElement(settings, "br", {});
-
-  function action_set_rate(rate) {
-    if(typeof rate == "string" && rate == "reset") {
-      _conf.override_playback_rate = _conf.default_playback_rate;
-      _conf.override_playback = false;
-      settings_span_playback_rate.innerText = _conf.default_playback_rate;
-    } else if(typeof rate == "number") {
-      _conf.override_playback_rate = rate;
-      _conf.override_playback = true;
-      settings_span_playback_rate.innerText = _conf.override_playback_rate;
+function menu() {
+  const init_playback_rate = _conf.default_playback_rate;
+  function action_toggle_pause() {
+    if(_conf.isEnabled) {
+      _conf.isEnabled = false;
+      _conf.default_playback_rate = 1;
+      yt_set_playback_rate();
+      GM_unregisterMenuCommand("Pause");
+      GM_registerMenuCommand("Resume", action_toggle_pause);
+    } else {
+      _conf.isEnabled = true;
+      _conf.default_playback_rate = init_playback_rate;
+      yt_set_playback_rate();
+      GM_unregisterMenuCommand("Resume");
+      GM_registerMenuCommand("Pause", action_toggle_pause);
     }
   }
-
-  GM_addElement(settings, "label", { textContent: "override: "});
-  ["reset", 0.75, 1, 1.25, 1.5, 1.75].forEach(function(r) {
-    const btn = GM_addElement(settings, "button", {
-      class: "__us_btn",
-      textContent: String(r),
-    });
-    btn.onclick = function() {action_set_rate(r);}
-  });
-
-  const overlay = GM_addElement("div", { class: "__us_overlay"});
-  overlay.addEventListener("click", function() {
-    settings.style.display = "none";
-    overlay.style.display = "none";
-  });
-
-  function action_show_settings() {
-    settings.style.left = String((innerWidth - 150) / 2) + "px";
-    settings.style.display = "block";
-    overlay.style.display = "block";
-  }
-  GM_registerMenuCommand("Settings", action_show_settings);
+  GM_registerMenuCommand(_conf.isEnabled ? "Pause" : "Resume", action_toggle_pause);
 }
 
 function with_debounce(func) {
@@ -106,9 +61,6 @@ function yt_set_playback_rate() {
   var elem_channel = document.querySelector(_conf.elem.channel);
   if (elem_vid && elem_channel) {
     var pbr = _conf.preset[elem_channel.getAttribute("href")] || _conf.default_playback_rate;
-    if(_conf.override_playback) {
-      pbr = _conf.override_playback_rate;
-    }
     if(pbr != elem_vid.playbackRate) {
       elem_vid.playbackRate = pbr;
     }
@@ -126,5 +78,5 @@ function yt_set_playback_rate() {
     subtree: true
   });
 
-  inject_ui();
+  menu();
 })();
